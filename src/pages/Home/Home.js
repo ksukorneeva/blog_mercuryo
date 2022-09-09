562b35d8e0049ced18f6c5af43a127d1ef9955f8
Ксения Корнеева <user@MacBook-Pro-User.local>
2022-09-09 18:47:32 +0300

    8

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -24,11 +24,10 @@ const Desctop = () => {
 
     const gettingPosts = useCallback(async () => {
         const data = await axios.get('/posts');
-        const posts = data.data;
-        const articles = posts.map((item) =>
+        const posts = data.data.map((item) =>
             item.x_categories.split(',').includes('Articles') ? item : ''
         );
-        setArticlePosts(articles);
+        setArticlePosts(posts);
         // console.log(categoryFilter(posts, 'Articles'));
         // setInsightsPosts(categoryFilter(posts, 'Insights'));
         // setSuccessPosts(categoryFilter(posts, 'Success stories'));
